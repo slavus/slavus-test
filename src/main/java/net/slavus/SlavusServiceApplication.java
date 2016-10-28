c7299bf9c5703ae13434e35202c5a44c332c6263
Hrvoje Slaviček <slavus@gmail.com>
2016-10-28 08:53:47 +0200

    Initial commit from the atomist bot

---
+++ src/main/java/net/slavus/SlavusServiceApplication.java
@@ -0,0 +1,12 @@
+package net.slavus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlavusServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlavusServiceApplication.class, args);
+    }
+}
